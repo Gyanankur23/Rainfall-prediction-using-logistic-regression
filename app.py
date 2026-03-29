2eac87957843dd147779bbcc5c787601e9e6e8e4
Gyanankur Baruah <gyanankurcricket20@gmail.com>
2026-03-29 23:44:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_absolute_error
 
+
 st.set_page_config(page_title="Rainfall Prediction India", layout="wide")
 
 # Regions and States mapping 
